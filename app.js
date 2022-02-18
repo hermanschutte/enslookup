15996b968e8ebac8d9e9f93f0f9220c42082fec3
Herman Schutte <herman.schutte@gmail.com>
2022-02-18 18:19:17 +0200

    Add get text records

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 
 var indexRouter = require('./routes/index');
+
 var textRecordsRouter = require('./routes/text-records');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/text-records', textRecordsRouter);
@@ -22,7 +31,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = !process.env.NODE_ENV === 'production' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
--- routes/text-records.js
+++ routes/text-records.js
@@ -1,8 +1,65 @@
+var createError = require('http-errors');
 var express = require('express');
 var router = express.Router();
+const { ethers } = require('ethers');
+
+router.get('/', function(req, res, next) {
+    res.send('');
+});
 
 router.get('/:address', function(req, res, next) {
-    res.send(req.params.address);
+    if (!req.params.address.endsWith('.eth')) {
+        return next(createError(400, 'Invalid Address'));
+    }
+
+    const network = 'homestead'; // mainnet
+
+    const provider = new ethers.providers.InfuraProvider(network, {
+        infura: {
+            projectId: process.env.INFURA_ID,
+            projectSecret: process.env.INFURA_SECRET,
+        }
+    });
+
+    const keys = [
+        'avatar',
+        'description',
+        'display',
+        'email',
+        'keywords',
+        'mail',
+        'notice',
+        'location',
+        'phone',
+        'url',
+        'com.discord',
+        'com.github',
+        'com.linkedin',
+        'com.twitter',
+        'io.keybase',
+        'org.telegram',
+    ];
+
+    const getTextRecords = async () => {
+        const resolver = await provider.getResolver(req.params.address);
+        
+        const results = await Promise.all(keys.map(async (key) => (
+            {
+                key: key,
+                value: await resolver.getText(key)
+            }
+        )));
+
+        const records = results.reduce((obj, item) => (obj[item.key] = item.value, obj), {});
+
+        records['address'] = await resolver.getAddress();
+
+        return records;
+    };
+
+    getTextRecords().then((response) => {
+        res.json(response);
+    })
 });
 
 module.exports = router;
